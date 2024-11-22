5a6ff0ddbdaa515119b887e26a7bb1b577db8fdd
k67sam@gmail.com <37092889+sam67k@users.noreply.github.com>
2024-11-22 12:45:08 +0500

    code cleanup

--- app/utils/assetUrl.js
+++ app/utils/assetUrl.js
@@ -1,28 +1,14 @@
-const { ASSET_URL_TYPE } = require("../constants");
+const { ASSET_PATH_MAPPING } = require("../constants");
 
 function getAssetUrl(req, assetType, assetPath) {
   const baseUrl = `${req.protocol}://${req.get("host")}`;
+  const relativePath = ASSET_PATH_MAPPING[assetType];
 
-  switch (assetType) {
-    case ASSET_URL_TYPE.userProfileImage:
-      return `${baseUrl}/assets/images/profiles/${assetPath}`;
-    case ASSET_URL_TYPE.productBannerImage:
-      return `${baseUrl}/assets/images/products/banners/${assetPath}`;
-    case ASSET_URL_TYPE.eventBannerImage:
-      return `${baseUrl}/assets/images/events/banners/${assetPath}`;
-    case ASSET_URL_TYPE.mediaAndNewsBannerImage:
-      return `${baseUrl}/assets/images/mediaAndNews/banners/${assetPath}`;
-    case ASSET_URL_TYPE.articleBannerImage:
-      return `${baseUrl}/assets/images/articles/banners/${assetPath}`;
-    case ASSET_URL_TYPE.artcileAttachmentDocument:
-      return `${baseUrl}/assets/documents/articles/attachments/${assetPath}`;
-    case ASSET_URL_TYPE.productMSDSDocument:
-      return `${baseUrl}/assets/documents/products/msds/${assetPath}`;
-    case ASSET_URL_TYPE.productTDSDocument:
-      return `${baseUrl}/assets/documents/products/tds/${assetPath}`;
-    default:
-      throw new Error(`Unknown asset type: ${assetType}`);
+  if (!relativePath) {
+    throw new Error(`Unknown asset type: ${assetType}`);
   }
+
+  return `${baseUrl}/assets/${relativePath}/${assetPath}`;
 }
 
 module.exports = getAssetUrl;
