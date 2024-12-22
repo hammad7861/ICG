f884b7082752342cb7c9a79ac62d53a5b1f5caa3
samama <37092889+sam67k@users.noreply.github.com>
2024-12-22 22:13:00 +0500

    added dashboard frontend

---
+++ Client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    screens:{
+     "custom-3xl":"1468px"
+    },
+    extend: {},
+  },
+  plugins: [],
+};
+
