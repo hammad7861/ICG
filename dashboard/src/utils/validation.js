d8e027d1cf92d9e08cdd3b4a98875fa8ebec1d29
samama <37092889+sam67k@users.noreply.github.com>
2025-02-19 21:45:17 +0500

    quick fix

--- dashboard/src/utils/validation.js
+++ dashboard/src/utils/validation.js
@@ -3,19 +3,19 @@ import * as Yup from "yup";
 const bulkAddProductValidationSchema = Yup.object().shape({
 	Products: Yup.string().required("Product name is required"),
 
-	"Chemical Name": Yup.string().nullable(),
+	"Chemical Name": Yup.string().nullable().notRequired(),
 
-	"Chemical Formula": Yup.string().nullable(),
+	"Chemical Formula": Yup.string().nullable().notRequired(),
 
-	"Molecular Weight": Yup.string().nullable(),
+	"Molecular Weight": Yup.string().nullable().notRequired(),
 
-	"CAS No.": Yup.string().nullable(),
+	"CAS No.": Yup.string().nullable().notRequired(),
 
-	Description: Yup.string().nullable(),
+	Description: Yup.string().nullable().notRequired(),
 
-	category: Yup.string().nullable(),
+	category: Yup.string().nullable().notRequired(),
 
-	"Product Form": Yup.string().nullable(),
+	"Product Form": Yup.string().nullable().notRequired(),
 });
 
 export default bulkAddProductValidationSchema;
